a87b50a5e018bf77d992ac8094a7184a9cb31ced
Paweł Jędrzejewski <pawel@sylius.org>
2015-08-27 15:03:37 +0200

    [Step 10] Implement the UI context

--- features/bootstrap/UiContext.php
+++ features/bootstrap/UiContext.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Domain\Book;
+use App\Domain\BookTitle;
+use App\Domain\Isbn;
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Behat\Hook\Scope\BeforeScenarioScope;
 use Behat\MinkExtension\Context\MinkContext;
@@ -27,6 +30,40 @@ class UiContext extends MinkContext implements SnippetAcceptingContext, KernelAw
         $purger->purge();
 
         $entityManager->clear();
+    }
+
+    /**
+     * @Given a book with ISBN :isbn and title :title was added to the catalog
+     */
+    public function aBookWithIsbnAndTitleWasAddedToTheCatalog($isbn, $title)
+    {
+        $this->getCatalog()->add(Book::withTitleAndIsbn(new BookTitle($title), new Isbn($isbn)));
+    }
+
+    /**
+     * @When I search catalog by ISBN number :isbn
+     */
+    public function iSearchCatalogByIsbnNumber($isbn)
+    {
+        $this->visit('/');
+        $this->fillField('ISBN', $isbn);
+        $this->pressButton('Search');
+    }
+
+    /**
+     * @Then I should find a single book
+     */
+    public function iShouldFindASingleBook()
+    {
+        $this->assertPageContainsText('Found 1 result(s).');
+    }
+
+    /**
+     * @return \App\Domain\BookCatalog\BookCatalogInterface
+     */
+    private function getCatalog()
+    {
+        return $this->get('app.catalog');
     }
 
     private function get($id)
--- src/App/Bundle/Controller/DefaultController.php
+++ src/App/Bundle/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace App\Bundle\Controller;
 
+use App\Domain\BookCatalog\BookCatalogInterface;
+use App\Domain\Isbn;
 use Symfony\Component\DependencyInjection\ContainerAware;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,8 +14,22 @@ class DefaultController extends ContainerAware
     {
         $parameters = array();
 
+        if ($request->query->has('query')) {
+            $query = $request->query->get('query');
+
+            $parameters['results'] = $this->getCatalog()->searchByIsbn(new Isbn($query));
+        }
+
         $html = $this->container->get('twig')->render('default/homepage.twig', $parameters);
 
         return new Response($html);
+    }
+
+    /**
+     * @return BookCatalogInterface
+     */
+    private function getCatalog()
+    {
+        return $this->container->get('app.catalog');
     }
 }
